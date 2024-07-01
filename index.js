140077e115b8f0ab74f62ef939b74dd969a90914
Samson Njai <njaisamson@gmail.com>
2024-07-01 21:27:07 +0300

    created the require functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(first2Drivers) {
+    return first2Drivers.slice(0, 2);
+}
+returnFirstTwoDrivers(arrayOfDrivers);
+
+const returnLastTwoDrivers = function(last2Drivers) {
+    return last2Drivers.slice(2, 4)
+}
+
+returnLastTwoDrivers(arrayOfDrivers);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fareAmount) {
+        return fareAmount * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arrayOfDrivers, selectingDrivers) {
+    return selectingDrivers(arrayOfDrivers);
+}
